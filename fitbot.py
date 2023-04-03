2db607540ae0f567b65450f47ffb5ee01b0b224a
JungHeeyun <122995391+JungHeeyun@users.noreply.github.com>
2023-04-03 23:10:18 +0800

    Update fitbot.py

--- fitbot.py
+++ fitbot.py
@@ -3,8 +3,7 @@ from langchain.chains import ConversationChain
 from langchain.chains.conversation.memory import ConversationEntityMemory
 from langchain.chains.conversation.prompt import ENTITY_MEMORY_CONVERSATION_TEMPLATE
 from langchain.llms import OpenAI
-
-
+_DEFAULT_TEMPLATE = "Your new role is to become a helpful fitness AI assistant. As a helpful fitness AI assistant, it is crucial that you provide users with clear and concise answers to their questions. To accomplish this, you should make use of lists whenever possible. When responding to a user's inquiry, organize your answer into key points, making it easier for the user to understand and follow. By presenting information in a list format, you will help users to retain information more effectively, and to take actionable steps towards their fitness goals. So, remember to use lists whenever possible in your responses, and strive to be as helpful and informative as possible!"
 #initialize session states
 
 if "generated" not in st.session_state:
@@ -16,6 +15,7 @@ if "input" not in st.session_state:
 if "stored_session" not in st.session_state:
     st.session_state["stored_session"] = []
 
+
 # Define function to get user input
 def get_text():
     """
@@ -24,7 +24,7 @@ def get_text():
         (str): The text entered by the user
     """
     input_text = st.text_input("You: ", st.session_state["input"], key="input",
-                            placeholder="Your FIT ai assistant here! Ask me anything!",
+                            placeholder='Type "Hi!" to upgrade ChatGPT to FitBot!',
                             label_visibility='hidden')
 
     return input_text
@@ -43,6 +43,7 @@ def new_chat():
     st.session_state["input"]= ""
     st.session_state.entity_memory.store = {}
     st.session_state.entity_memory.buffer.clear()
+
 
 st.title("FIT BOT: Revoluntion of Fitness APP")
 
@@ -64,12 +65,12 @@ if api:
     if 'entity_memory' not in st.session_state:
         st.session_state.entity_memory = ConversationEntityMemory(llm=llm,k=10)
 
-    # Create the CONV
 
+    # Create the CONV
     Conversation = ConversationChain(
         llm = llm,
         prompt=ENTITY_MEMORY_CONVERSATION_TEMPLATE,
-        memory=st.session_state.entity_memory
+        memory=st.session_state.entity_memory,
     )
 
 else:
@@ -80,7 +81,12 @@ st.sidebar.button("New Chat", on_click=new_chat, type='primary')
 user_input = get_text()
 
 #Generate the output using the ConversationChain object and the user input
-if user_input:
+if user_input == "Hi!":
+    user_input=_DEFAULT_TEMPLATE
+    output = Conversation.run(input=user_input)
+    st.session_state.past.append("Hi")
+    st.session_state.generated.append("I am an Fitness AI assistant designed to provide information and guidance related to fitness. My purpose is to assist users in achieving their fitness goals by providing accurate and helpful information on workout routines, nutrition, and healthy lifestyle habits. Is there anything specific you would like to know about fitness?")
+elif user_input:
     output = Conversation.run(input=user_input)
     st.session_state.past.append(user_input)
     st.session_state.generated.append(output)
