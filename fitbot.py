de3cf0dde48dc0928b96b834a7d822cae786f774
JungHeeyun <122995391+JungHeeyun@users.noreply.github.com>
2023-04-29 02:59:33 +0800

    Update fitbot.py

--- fitbot.py
+++ fitbot.py
@@ -86,7 +86,7 @@ if password1 == "gmldbs8132":
 if st.session_state["membership_upgraded"]:
     google_api_key = "AIzaSyAWyB91DSW2cDXy0ggWSDCFh2k7X3tlJ_s"
     custom_search_engine_id = "a1d21c0cdf2ea4339"
-    api = "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"
+    api = "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"
     MODEL = 'gpt-3.5-turbo'
 else:
     api = st.sidebar.text_input("OpenAI API-Key", type="password")
